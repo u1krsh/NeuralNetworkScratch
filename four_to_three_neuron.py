823d138284a7fde79f04d34b5585699334b6c59d
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2025-11-30 14:18:10 +0530

    Initial commit

---
+++ four_to_three_neuron.py
@@ -0,0 +1,11 @@
+inputs = [1, 2, 3, 2.5]
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0,26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bais1 = 2
+bais2 = 3
+bais3 = 0.5
+output = inputs[0]*weights[0] +inputs[1]*weights[1]+ inputs[2]*weights[2] +inputs[3]*weights[3] + bais 
+
+print(output)
---
+++ single_neuron.py
@@ -0,0 +1,5 @@
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bais = 2
+output = inputs[0]*weights[0] +inputs[1]*weights[1]+ inputs[2]*weights[2] +inputs[3]*weights[3] + bais 
+print(output)
